8602cb47770ee809e0ee8e5528841bfc160951df
James <domagals.jame@gmail.com>
2018-04-15 22:17:36 -0500

    intial commit

---
+++ src/main/java/org/chrono/exam/ExamGeneratorService/ExamGeneratorServiceApplication.java
@@ -0,0 +1,12 @@
+package org.chrono.exam.ExamGeneratorService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamGeneratorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamGeneratorServiceApplication.class, args);
+	}
+}
